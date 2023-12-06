b42e225282c58da5f8a1f143cb62680972610156
Kenji <viniciuskenji_2012@outlook.com>
2023-12-06 18:09:24 -0300

    Adicionando modo dark

--- script/index.js
+++ script/index.js
@@ -1,6 +1,11 @@
 const navbar = document.querySelector('.navbar');
 const mobileNavbar = document.querySelector('.navbar__mobile');
 const button = document.querySelector('.burguer');
+const dark = document.querySelector('#tema');
+const html = document.querySelector('html');
+const darkMobile = document.querySelector('#tema-mobile');
+
+
 
 button.addEventListener('click', function() {
     mobileNavbar.classList.toggle('active');
@@ -9,4 +14,14 @@ button.addEventListener('click', function() {
 window.addEventListener('scroll', function() {
     if(this.window.pageYOffset > 0) return navbar.classList.add('active');
     return navbar.classList.remove('active');
-})
+});
+
+dark.addEventListener('change', () => {
+    html.classList.toggle('dark-mode');
+});
+
+darkMobile.addEventListener('click', () => {
+    html.classList.toggle('dark-mode');
+});
+
+
